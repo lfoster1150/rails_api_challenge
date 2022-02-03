30fea6dcc3e467895975e51a87212307a41768fd
lfoster1150 <lfoster1150@gmail.com>
2022-02-03 14:32:15 -0600

    POST Request functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user.to_json(only: [:id, :first_name, :last_name, :email])
   end
+
+  def new
+    user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user.to_json(only: [:id, :first_name, :last_name, :email])
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
 end
